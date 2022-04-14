183f6804bf8b4a4199516dcaba359297017781d8
Song San <songsan2012@gmail.com>
2022-04-14 16:00:08 -0700

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by song on 4/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
